983acd3bae8c9e4e9a1603de59cd4345855c4361
Ritesh <ritesh9singh@gmail.com>
2018-08-08 12:49:17 +0530

    code update

---
+++ Gmail Inbox Read.py
@@ -0,0 +1,64 @@
+import base64
+import sys
+import imaplib
+import email
+import re
+import getpass
+import datetime
+import time
+from dateutil.relativedelta import relativedelta
+
+# comment this code if you want to store details in code.
+email_address = input('Email Address: ')
+password = print('Your password: ')+getpass.getpass()
+label = input("Please enter the gmail label name:")
+days = input("Read email in between: [new date,old date] [d-m-y,d-m-y] ex.[04-Jun-2018,02-Mar-2018]")
+
+# Uncomment the below code for storing details.
+# email_address = "abc@example.com"
+# password = "Your password"
+# label = "Jenkins"
+# days = "07-Jul-2018,31-Jun-2018"
+
+smtp_server = "imap.gmail.com"
+smpt_port = 993
+subject = []
+
+
+def filter_date(data):
+    global command
+
+    date = days.split(",")
+    from_date = date[0]
+    to_date = date[1]
+    command = 'SINCE "'+to_date+'" BEFORE "'+from_date+'"'
+
+
+def read_email_from_gmail():
+    filter_date(days)
+    mail = imaplib.IMAP4_SSL(smtp_server)
+    mail.login(email_address, password)
+    mail.select('"'+label+'"', readonly=True)
+    result, data = mail.uid('search', None, '('+command+')')
+    if result == 'OK':
+        print('Processing mailbox...')
+    else:
+        print("Reading error")
+        sys.exit(0)
+    ids = data[0].split()
+    if len(ids) == 0:
+        print("No email found in selected dates.")
+    for i in ids:
+        result, data = mail.uid('fetch', i, "(RFC822)")
+        raw_email = data[0]
+        f1 = re.split('Subject: ', str(raw_email))
+        f2 = re.split(r"\\r", f1[1])
+        sub = f2[0]
+        subject.append(sub)
+    return subject
+
+
+subjects = read_email_from_gmail()
+for i in subject:
+    print(i)
+    time.sleep(0.2)
--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-Read your inbox email and automate the boring stuff.
+Read your Gmail inbox using this script and automate the boring stuff.
 
-Note: Change the "username" and "password" value before using this script.
+Features:
+--------
 
-Author: Ritesh Singh [ritesh9singh@gmail.com]
+    > Read any label of gmail ex. 'Inbox'.
+    > Read specific duration emails.
+    > Show all emails subjects. You can also show data using few modification.
+
+How to use:
+----------
+    Linux:
+    ------
+        > Download the script.
+        > python Gmail Inbox Read.py
+        > It will ask you four question.
+            1. Your email address "email_address".
+            2. Your password "Password".
+            3. Your email label ex. Inbox.
+            4. Set the duration [new date,old date] [dd-mm-yy,dd-mm-yy] ex.[04-Jun-2018,02-Mar-2018]".
+        > If you don't want to fill the question details then comment the lines in code and set all the details in code itself. so next time it will not ask
+          you any question.*
+        > It's done and you will see the magic.
+    Windows:
+    -------
+        > Install python and run this script.
+                    OR
+        > Use executable file.
+Author
+------
+
+Ritesh Singh [ritesh9singh@gmail.com]
