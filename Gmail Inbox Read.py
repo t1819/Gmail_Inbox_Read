1904c7b2221c96a42839872162f28901409e4fe2
ritesh singh <ritesh9singh@gmail.com>
2019-03-18 00:20:01 +0530

    Added new features in script.

--- Gmail Inbox Read.py
+++ Gmail Inbox Read.py
@@ -6,24 +6,9 @@ import re
 import getpass
 import datetime
 import time
+import config
 from dateutil.relativedelta import relativedelta
 
-# comment this code if you want to store details in code.
-email_address = input('Email Address: ')
-password = getpass.getpass()
-label = input("Please enter the Gmail label name:")
-from_date = input("Please enter the FROM date: ex. 27-Jul-2018\n")
-to_date = input("Please enter the TO date. ex.01-Aug-2018\n")
-
-# Uncomment the below code for storing details.
-# email_address = "abc@example.com"
-# password = "Your password"
-# label = "Jenkins"
-# from_date = "07-Jul-2018"
-# to_date = "30-Jul-2018"
-
-smtp_server = "imap.gmail.com"
-smpt_port = 993
 subject = []
 
 
@@ -32,7 +17,14 @@ def filter_date(d1, d2):
 
     command = 'SINCE "'+d2+'" BEFORE "'+d1+'"'
 
-def read_emails():
+
+def read_emails(smtp_server, subject):
+    email_address = config.email
+    password = config.password
+    label = config.label
+    from_date = config.from_date
+    to_date = config.to_date
+
     filter_date(to_date, from_date)
     mail = imaplib.IMAP4_SSL(smtp_server)
     mail.login(email_address, password)
@@ -46,8 +38,8 @@ def read_emails():
     ids = data[0].split()
     if len(ids) == 0:
         print("No email found in selected dates.")
-    for i in ids:
-        result, data = mail.uid('fetch', i, "(RFC822)")
+    for x in ids:
+        result, data = mail.uid('fetch', x, "(RFC822)")
         raw_email = data[0]
         f1 = re.split('Subject: ', str(raw_email))
         f2 = re.split(r"\\r", f1[1])
@@ -56,13 +48,22 @@ def read_emails():
     return subject
 
 
-try:
-    subjects = read_emails()
-except Exception as e:
-    print('[+] Error =' + str(e))
-    input("Press enter to exit.")
-for i in subject:
-    print(i)
-    time.sleep(0.2)
+def main():
+    try:
+        smtp_server = "imap.gmail.com"
+        read_emails(smtp_server, subject)
+        return True
+    except Exception as e:
+        return '[+] Error =' + str(e)
 
-input("Press enter to exit.")
+
+if __name__ == '__main__':
+    status = main()
+    if status:
+        if len(subject) > 0:
+            for x in subject:
+                print(x)
+        print("\nScript run successfully!!")
+    input("Press enter to exit.")
+    sys.exit(0)
+
---
+++ config.py
@@ -0,0 +1,5 @@
+email = 'Your_email_ID@gmail.com'
+password = 'Your Password'
+label = 'Inbox'
+from_date = '01-Mar-2019'
+to_date = '17-Mar-2019'
