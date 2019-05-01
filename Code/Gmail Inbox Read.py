dce313f6d3286b8e46b2d776e8d63bd0c9f01205
Ritesh Singh <ritesh9singh@gmail.com>
2019-05-01 20:02:33 +0530

    code update.

--- Code/Gmail Inbox Read.py
+++ Code/Gmail Inbox Read.py
@@ -1,69 +1,56 @@
-import base64
 import sys
 import imaplib
-import email
 import re
-import getpass
-import datetime
 import time
 import config
-from dateutil.relativedelta import relativedelta
-
-subject = []
-
-
-def filter_date(d1, d2):
-    global command
-
-    command = 'SINCE "'+d2+'" BEFORE "'+d1+'"'
+'''
+    Description: Read your Gmail label for specific duration and print all the subjects.
+    Pre-requirements: Go to google account --> security --> enable "Less secure app access" option.
+    Use: set options in config.py file before using this script.
+'''
 
 
-def read_emails(smtp_server, subject):
-    email_address = config.email
-    password = config.password
-    label = config.label
-    from_date = config.from_date
-    to_date = config.to_date
+class EmailRead:
+    def read_emails(self):
+        try:
+            mail = imaplib.IMAP4_SSL(self.smtp_server)
+            mail.login(self.email_address, self.password)
+            mail.select(self.label, readonly=True)
+            result, data = mail.uid('search', None, self.command)
+            if result == 'OK':
+                print('Processing mailbox...')
+            else:
+                print("Reading error")
+                sys.exit(0)
+            ids = data[0].split()
+            if len(ids) == 0:
+                print("No email found in selected dates.")
+            for x in ids:
+                result, data = mail.uid('fetch', x, "(RFC822)")
+                raw_email = data[0]
+                f1 = re.split('Subject: ', str(raw_email))
+                f2 = re.split(r"\\r", f1[1])
+                sub = f2[0]
+                self.subject.append(sub)
+            return self.subject
+        except:
+            print("[+] Error in reading your %s label." % self.label)
 
-    filter_date(to_date, from_date)
-    mail = imaplib.IMAP4_SSL(smtp_server)
-    mail.login(email_address, password)
-    mail.select('"'+label+'"', readonly=True)
-    result, data = mail.uid('search', None, '('+command+')')
-    if result == 'OK':
-        print('Processing mailbox...')
-    else:
-        print("Reading error")
-        sys.exit(0)
-    ids = data[0].split()
-    if len(ids) == 0:
-        print("No email found in selected dates.")
-    for x in ids:
-        result, data = mail.uid('fetch', x, "(RFC822)")
-        raw_email = data[0]
-        f1 = re.split('Subject: ', str(raw_email))
-        f2 = re.split(r"\\r", f1[1])
-        sub = f2[0]
-        subject.append(sub)
-    return subject
-
-
-def main():
-    try:
-        smtp_server = "imap.gmail.com"
-        read_emails(smtp_server, subject)
-        return True
-    except Exception as e:
-        return '[+] Error =' + str(e)
+    def __init__(self):
+        self.subject = []
+        self.smtp_server = "imap.gmail.com"
+        self.email_address = config.email
+        self.password = config.password
+        self.label = '"'+config.label+'"'
+        self.from_date = config.from_date
+        self.to_date = config.to_date
+        self.command = '(SINCE "' + self.from_date + '" BEFORE "' + self.to_date + '")'
 
 
 if __name__ == '__main__':
-    status = main()
-    if status:
-        if len(subject) > 0:
-            for x in subject:
-                print(x)
-        print("\nScript run successfully!!")
-    input("Press enter to exit.")
-    sys.exit(0)
-
+    r1 = EmailRead()
+    data = r1.read_emails()
+    if len(data) > 0:
+        for i in data:
+            print(i)
+            time.sleep(.2)
